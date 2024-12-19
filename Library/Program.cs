7d54c49808946b421a2bfd5e0c246ec9dfc3e79f
harrysurin <harrysurin@mail.ru>
2024-12-19 20:05:30 +0300

    Installed EF SqlServer, JWT Auth and Swashbuckle packages to the web api project.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,41 +1,72 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryRepository.Models;
+using LibraryServices.Interfaces;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var connectionString = "Server=localhost\\SQLEXPRESS;Database=LibraryApp;Trusted_Connection=True;TrustServerCertificate=True";
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+
+
+builder.Services.AddTransient<IRepository<Author>, Repository<Author>>();
+builder.Services.AddTransient<IRepository<Book>, Repository<Book>>();
+builder.Services.AddTransient<IRepository<RentHistory>, Repository<RentHistory>>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<IAuthorServices, AuthorService>();
+builder.Services.AddTransient<IBookServices, BookService>();
+builder.Services.AddTransient<IRentHistoryServices, RentHistoryService>();
+
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "APIforSPA", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<LibraryContext>();
 
 var app = builder.Build();
+app.MapIdentityApi<User>();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
