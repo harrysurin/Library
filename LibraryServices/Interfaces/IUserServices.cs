0b059795660898d98a2b0ad5b124690db96d6388
harrysurin <harrysurin@mail.ru>
2024-12-23 11:03:58 +0300

    Added User Services with User Manager implementation.

---
+++ LibraryServices/Interfaces/IUserServices.cs
@@ -0,0 +1,21 @@
+using LibraryRepository.Models;
+using  Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LibraryServices.Interfaces
+{
+    public interface IUserServices
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<IdentityResult> AddAsync(User user, string password);
+        Task DeleteAsync(User user);
+        Task UpdateAsync(User user);
+        Task<User?> FindByIdAsync(Guid userId);
+        Task<User?> FindByNameAsync(string userName);
+        Task<User?> FindByEmailAsync(string email);
+        Task ChangePassword(User user, string oldPassword, string newPassword);
+        Task<bool> IsInRoleAsync(User user, string rolename);
+        Task AddToRoleAsync(User user, string rolename);
+    }
+}
---
+++ LibraryServices/Services/UserServices.cs
@@ -0,0 +1,37 @@
+using LibraryRepository.Models;
+using LibraryServices.Interfaces;
+using  Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class UserServices : IUserServices
+{
+    private UserManager<User> userManager;
+
+    public UserServices(UserManager<User> _userManager)
+    {
+        userManager = _userManager;
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync() => await userManager.Users.ToListAsync();
+
+    public async Task<IdentityResult> AddAsync(User user, string password) => await userManager.CreateAsync(user, password);
+
+    public async Task DeleteAsync(User user) => await userManager.DeleteAsync(user);
+
+    public async Task UpdateAsync(User user) => await userManager.UpdateAsync(user);
+
+    public async Task<User?> FindByIdAsync(Guid userId) => await userManager.FindByIdAsync(userId.ToString());
+
+    public async Task<User?> FindByNameAsync(string userName) => await userManager.FindByNameAsync(userName);
+
+    public async Task<User?> FindByEmailAsync(string email) => await userManager.FindByEmailAsync(email);
+
+    public async Task ChangePassword(User user, string oldPassword, string newPassword)
+            => await userManager.ChangePasswordAsync(user, oldPassword, newPassword);
+
+    public async Task<bool> IsInRoleAsync(User user, string rolename) 
+            => await userManager.IsInRoleAsync(user, rolename);
+    
+    public async Task AddToRoleAsync(User user, string rolename) 
+            => await userManager.AddToRoleAsync(user, rolename);
+}
