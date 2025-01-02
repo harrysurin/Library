8974b038a79d9bc6f968e4e0654a7db7ef8a24c6
harrysurin <harrysurin@mail.ru>
2025-01-02 19:45:33 +0300

    Added to Author the next properties: Lastname and Country.

---
+++ LibraryRepository/Migrations/20250102153227_UpdatedAuthor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Authors",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Authors");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Authors",
+                newName: "Name");
+        }
+    }
+}
--- LibraryRepository/Models/Author.cs
+++ LibraryRepository/Models/Author.cs
@@ -7,7 +7,11 @@ public class Author
     [Key]
     public Guid AuthorId {get; set;} = Guid.NewGuid();
 
-    public string? Name {get; set;}
+    public string FirstName {get; set;}
+
+    public string LastName {get; set;}
 
     public DateTime? DateOfBirth {get; set;}
+
+    public string Country {get; set; }
 }
