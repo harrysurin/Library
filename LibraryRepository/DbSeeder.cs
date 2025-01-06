d3f8420a6b8e2e0653930eb2644945ef8fe22c50
harrysurin <harrysurin@mail.ru>
2025-01-06 18:05:36 +0300

    Removed commented code in DbSeeder.

--- LibraryRepository/DbSeeder.cs
+++ LibraryRepository/DbSeeder.cs
@@ -19,28 +19,5 @@ public class DbSeeder
         );
     }
 
-    // public void SeedAdminUser()
-    // {
-    //     var user = new User
-    //     {
-    //         Id = new Guid("37794e22-6ed1-4fd6-ace4-a69fb910d2a0"), // primary key
-    //         UserName = "admin",
-    //         NormalizedUserName = "ADMIN",
-    //     };
 
-    //     var hasher = new PasswordHasher<User>();
-    //     user.PasswordHash = hasher.HashPassword(user, "Pa$$w0rd");
-
-    //     //Seeding the User to AspNetUsers table
-    //     modelBuilder.Entity<User>().HasData(user);
-
-    //     //Seeding the relation between our user and role to AspNetUserRoles table
-    //     modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(
-    //         new IdentityUserRole<Guid>
-    //         {
-    //             RoleId = new Guid("729e2c22-3832-4ab5-8f28-5a2c99d85944"), 
-    //             UserId = new Guid("37794e22-6ed1-4fd6-ace4-a69fb910d2a0")
-    //         }
-    //     );
-    // }
 }
